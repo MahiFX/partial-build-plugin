e20a68e52fefedcde313d2c78991eccdb7f8ff9d
Lee Butts <lee.butts@mahifx.com>
2019-10-07 14:31:57 +1300

    Add mojo config prop

--- src/main/java/com/lesfurets/maven/partial/mojos/ChangedMojo.java
+++ src/main/java/com/lesfurets/maven/partial/mojos/ChangedMojo.java
@@ -1,24 +1,28 @@
 package com.lesfurets.maven.partial.mojos;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.*;
-import org.apache.maven.plugins.annotations.*;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.project.MavenProject;
-import org.eclipse.jgit.api.errors.GitAPIException;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.lesfurets.maven.partial.core.*;
 import com.lesfurets.maven.partial.utils.MavenToPlexusLogAdapter;
 import com.lesfurets.maven.partial.utils.PluginUtils;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.StringJoiner;
 
 @Mojo(name = "writeChanged", defaultPhase = LifecyclePhase.VALIDATE,
-                threadSafe = true, inheritByDefault = false, aggregator = true)
+        threadSafe = true, inheritByDefault = false, aggregator = true)
 public class ChangedMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}")
@@ -34,7 +38,7 @@ public class ChangedMojo extends AbstractMojo {
     public String key;
 
     @Parameter(required = false, property = Property.PREFIX + "referenceBranch", defaultValue =
-                    "refs/remotes/origin/develop")
+            "refs/remotes/origin/develop")
     public String referenceBranch;
 
     @Parameter(required = false, property = Property.PREFIX + "baseBranch", defaultValue = "HEAD")
@@ -62,7 +66,7 @@ public class ChangedMojo extends AbstractMojo {
     public boolean fetchReferenceBranch;
 
     @Parameter(required = false, property = Property.PREFIX + "outputFile", defaultValue = "${project" +
-                    ".basedir}/changed.projects")
+            ".basedir}/changed.projects")
     public String outputFile;
 
     @Parameter(required = false, property = Property.PREFIX + "writeChanged", defaultValue = "false")
@@ -70,6 +74,9 @@ public class ChangedMojo extends AbstractMojo {
 
     @Parameter(required = false, property = Property.PREFIX + "ignoreChanged")
     public String ignoreChanged;
+
+    @Parameter(required = false, property = Property.PREFIX + "ignoredFiles")
+    public String ignoredFiles;
 
     @Parameter(required = false, property = Property.PREFIX + "buildSnapshotDependencies", defaultValue = "false")
     public String buildSnapshotDependencies;
@@ -104,8 +111,8 @@ public class ChangedMojo extends AbstractMojo {
 
             PluginUtils.writeChangedProjectsToFile(sortedChanged, new File(outputFile));
             session.getProjects().forEach(m -> m.getProperties()
-                            .setProperty(UnchangedProjectsRemover.CHANGED_PROJECTS, PluginUtils.joinProjectIds
-                                            (sortedChanged, new StringJoiner(",")).toString()));
+                    .setProperty(UnchangedProjectsRemover.CHANGED_PROJECTS, PluginUtils.joinProjectIds
+                            (sortedChanged, new StringJoiner(",")).toString()));
         } catch (GitAPIException | IOException e) {
             throw new MojoExecutionException("Exception during Partial Build execution: ", e);
         }
